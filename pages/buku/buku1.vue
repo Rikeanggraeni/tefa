43d89ac04196b250c974d16dd0f5750ed8b0c7b1
Rikeanggraeni <142281813+Rikeanggraeni@users.noreply.github.com>
2024-02-28 15:37:12 +0700

    Add files via upload

---
+++ pages/buku/buku1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-2">
+        <div class="card mb-3">
+          <div class="card-body">
+            <img src="assets/img/sobatsakit.jpeg" class="sobatsakit" alt="sobatsakit">
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="col p-5"><h1 style="font-size: 40px;">Sobat Sakit</h1></div>
+        <h4>Penulis: </h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.cover {
+  height: 300px;
+  width: 200px;
+}
+.card.mb-3 {
+  margin-top: 50px;
+  margin-left: 20px;
+  height: 340px;
+  width: 235px;
+}
+h3 {
+  margin-top: 60px;
+  margin-left: 20px;
+}
+img {
+  height: 300px;
+  width: 200px;
+}
+</style>
